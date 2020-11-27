9ed3121683bc5b2994f57b37488f673c0b342afa
alextakahashi <alextakahashi@berkeley.edu>
2020-11-26 19:02:22 -0800

    Exercise 1-2

---
+++ Chapter1/1-2/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+Adding Escape Character \c.  Recieve warning unknown escape sequence: '\c'
+*/
+
+int main()
+{
+    printf("Hello World \c blah blah");
+
+    return 0;
+}
