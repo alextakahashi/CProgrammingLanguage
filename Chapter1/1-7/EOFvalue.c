150dd92b46db037c4348429d7b8269e851fe3e75
alextakahashi <alextakahashi@berkeley.edu>
2020-11-26 19:38:48 -0800

    Create EOFvalue.c

---
+++ Chapter1/1-7/EOFvalue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Printing the value of EOF.  EOF is a macro defined in stdio.h.
+ * See http://www.learntosolveit.com/cprogramming/Ex_1.7_eofval.html#:~:text=Since%20EOF%20is%20an%20integer,d%20format%20in%20the%20printf.&text=EOF%20value%20is%20printed%20as%20%2D1.
+ * */
+
+int main()
+{
+    printf("The value of EOF is %d.",EOF);
+    return 0;
+}
